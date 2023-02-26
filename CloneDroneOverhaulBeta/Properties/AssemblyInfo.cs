e395af0e02a0eadda812f149486670a0ac5bcddd
A TVCat <ai.grishanoff@yandex.ru>
2023-02-26 19:57:41 +0300

    a0.3.0.51

--- CloneDroneOverhaulBeta/Properties/AssemblyInfo.cs
+++ CloneDroneOverhaulBeta/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Clone Drone Overhaul BETA")]
-[assembly: AssemblyDescription("A mod that changes the entire clone drone game!")]
+[assembly: AssemblyTitle("Clone Drone Overhaul Preview")]
+[assembly: AssemblyDescription("A mod, that changes everything in Clone drone game!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Clone Drone Overhaul Mod")]
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("faaf63a3-61f4-43f3-860c-b9c2c98b6f3b")]
-[assembly: AssemblyVersion("0.3.0.49")]
+[assembly: AssemblyVersion("0.3.0.51")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
