15aab65b7fccdf5e912606259484b9c4ff3cfff5
A TVCat <ai.grishanoff@yandex.ru>
2024-07-22 02:01:16 +0300

    merge conficts resolved

--- OverhaulMod/Patches/SeveredVolumeGenerator_Patch.cs
+++ OverhaulMod/Patches/SeveredVolumeGenerator_Patch.cs
@@ -11,18 +11,16 @@ namespace OverhaulMod.Patches
         [HarmonyPatch("CreateVolumeCopy")]
         private static void CreateVolumeCopy_Postfix(SeveredBodyPart __result)
         {
-<<<<<<< HEAD:CloneDroneOverhaulBeta/Internal/Patches/SeveredVolumeGenerator_Patch.cs
             if (Random.Range(0, 10) < 5)
             {
-=======
-            if (SeveredBodyPartSparks.EnableGarbageParticles && Random.value <= 0.7f)
->>>>>>> 0.4.x-development:OverhaulMod/Patches/SeveredVolumeGenerator_Patch.cs
-                _ = __result.gameObject.AddComponent<SeveredBodyPartSparks>();
+                if (SeveredBodyPartSparks.EnableGarbageParticles && Random.value <= 0.7f)
+                    _ = __result.gameObject.AddComponent<SeveredBodyPartSparks>();
 
-            Rigidbody rigidBody = __result.gameObject.GetComponent<Rigidbody>();
-            if (rigidBody)
-            {
-                rigidBody.interpolation = RigidbodyInterpolation.Interpolate;
+                Rigidbody rigidBody = __result.gameObject.GetComponent<Rigidbody>();
+                if (rigidBody)
+                {
+                    rigidBody.interpolation = RigidbodyInterpolation.Interpolate;
+                }
             }
         }
     }
