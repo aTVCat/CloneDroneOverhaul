c3aca704c3823e03c7fdb16ee56ee3de2adefa18
A TVCat <ai.grishanoff@yandex.ru>
2024-07-29 17:32:37 +0300

    what

--- OverhaulMod/Patches/SeveredVolumeGenerator_Patch.cs
+++ OverhaulMod/Patches/SeveredVolumeGenerator_Patch.cs
@@ -11,16 +11,13 @@ namespace OverhaulMod.Patches
         [HarmonyPatch(nameof(SeveredVolumeGenerator.CreateVolumeCopy))]
         private static void CreateVolumeCopy_Postfix(SeveredBodyPart __result)
         {
-            if (Random.Range(0, 10) < 5)
-            {
-                if (SeveredBodyPartSparks.EnableGarbageParticles && Random.value <= 0.7f)
-                    _ = __result.gameObject.AddComponent<SeveredBodyPartSparks>();
+            if (SeveredBodyPartSparks.EnableGarbageParticles && Random.value <= 0.7f)
+                _ = __result.gameObject.AddComponent<SeveredBodyPartSparks>();
 
-                Rigidbody rigidBody = __result.gameObject.GetComponent<Rigidbody>();
-                if (rigidBody)
-                {
-                    rigidBody.interpolation = RigidbodyInterpolation.Interpolate;
-                }
+            Rigidbody rigidBody = __result.gameObject.GetComponent<Rigidbody>();
+            if (rigidBody)
+            {
+                rigidBody.interpolation = RigidbodyInterpolation.Interpolate;
             }
         }
     }
