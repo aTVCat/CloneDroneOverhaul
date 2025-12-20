c1573105184a31acbd8a668f5cb509489eb2f6c4
A TVCat <ai.grishanoff@yandex.ru>
2025-12-20 14:16:54 +0300

    build

--- OverhaulMod/UI/UIPatchNotes.cs
+++ OverhaulMod/UI/UIPatchNotes.cs
@@ -364,7 +364,7 @@ namespace OverhaulMod.UI
                     return $"{patch} 7";
                 case "4.2.1045":
                     return $"{patch} 8";
-                case "4.2.1048":
+                case "4.2.1049":
                     return $"{patch} 9";
 
                 default:
